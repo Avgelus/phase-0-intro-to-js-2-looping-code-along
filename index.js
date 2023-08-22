f7101889fc05da74006c691ad8944bf4f61f3949
+ <+>
2023-08-22 02:17:52 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+const Cards = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(Cards) {
+    const thankYouMessages = []; 
+    
+    for (let i = 0; i < Cards.length; i++) {
+        const message = `Thank you, ${Cards[i]}, for the wonderful surprise gift!`;
+        thankYouMessages.push(message); 
+        console.log(message); 
+    }
+
+    return thankYouMessages;
+}
+
+function countDown(startingNumber) {
+    for (let i = startingNumber; i >= 0; i--) {
+        console.log(i);
+    }
+}
